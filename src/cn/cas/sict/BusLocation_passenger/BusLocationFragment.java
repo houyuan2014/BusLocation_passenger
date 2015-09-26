610749beeea65e1b99de78fd19844f1f6348da92
hy <houyuan@qq.com>
2015-09-26 19:44:51 +0800

    v0.1

--- src/cn/cas/sict/BusLocation_passenger/BusLocationFragment.java
+++ src/cn/cas/sict/BusLocation_passenger/BusLocationFragment.java
@@ -1,73 +1,153 @@
 package cn.cas.sict.BusLocation_passenger;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cn.cas.sict.utils.HttpUtil;
+import cn.cas.sict.utils.ToastUtil;
+
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationListener;
 import com.amap.api.location.LocationManagerProxy;
 import com.amap.api.location.LocationProviderProxy;
 import com.amap.api.maps2d.AMap;
-import com.amap.api.maps2d.LocationSource;
+import com.amap.api.maps2d.CameraUpdateFactory;
 import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.CameraPosition;
 import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.maps2d.model.Marker;
 import com.amap.api.maps2d.model.MarkerOptions;
 
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class BusLocationFragment extends Fragment implements LocationSource,
+public class BusLocationFragment extends Fragment implements OnClickListener,
 		AMapLocationListener {
 
-	AMap aMap;
+	static final CameraPosition SHENYANG = new CameraPosition(new LatLng(
+			123.43, 41.80), 10, 0, 0);
+	Button btUserLoc;
+	AMap map;
 	MapView mapView;
-	MarkerOptions markeroption;
-	Marker marker;
-	// DrivePath myDrivepath;DriveStep myDrivestep;List<LatLonPoint> myPolyline;
-	OnLocationChangedListener mListener;
-	LocationManagerProxy mAMapLocationManager;
+	LatLng userLatLng, busLatLng;
+	Map<String, String> userLocMap;
+	String driverLocDes, flag;
+	int a = 1;
+	MarkerOptions driverMarkerOption, userMarkerOption;
+	Marker drvierMarker, myMarker;
+	LocationManagerProxy mapLocationManager;
+	private boolean isUserlocValid, hasMoveCamera;
+	Timer timer;; // 定义定时器、定时器任务及Handler句柄
+	MyTimerTask timerTask;
+	MyHandler handler;
 
+	class MyTimerTask extends TimerTask {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			for (; !isUserlocValid;)
+				;
+			handler.sendEmptyMessage(3);
+		}
+	};
+
+	class MyHandler extends Handler {
+		public void handleMessage(Message msg) {
+			if (msg.what == 3) { // 执行定时任务
+				requestDriverLoc();// 发送HTTp请求
+				if (!hasMoveCamera) {
+					map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+							busLatLng, 15));
+					hasMoveCamera = true;
+				}
+			}
+
+		}
+	};
+
+	// DrivePath myDrivepath;DriveStep myDrivestep;List<LatLonPoint> myPolyline;
+	// OnLocationChangedListener mListener;
+
+	// 创建fragment时被回调，该方法只会被调用一次
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		markeroption = new MarkerOptions();
+		timer = new Timer();
+		handler = new MyHandler();
+		isUserlocValid = false;
+		hasMoveCamera = false;
+		userLocMap = new HashMap<String, String>();
+		driverMarkerOption = new MarkerOptions();
+		userMarkerOption = new MarkerOptions();
+		Log.i("aaa", "fra create");
 	}
 
+	// 每次创建，绘制该fragment的view组件时回调该方法
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-
-		Log.i("aaa", "111111");
+		// 加载布局文件
 		View rootView = inflater.inflate(R.layout.fragment_bus_location,
 				container, false);
-		Log.i("aaa", "22222");
-
+		btUserLoc = (Button) rootView.findViewById(R.id.bt_userloc);
+//		btUserLoc.setOnKeyListener(new OnKeyListener() {
+//			
+//			@Override
+//			public boolean onKey(View arg0, int arg1, KeyEvent arg2) {
+//				// TODO Auto-generated method stub
+//				return false;
+//			}
+//		});
 		mapView = (MapView) rootView.findViewById(R.id.map);
 		mapView.onCreate(savedInstanceState);
-		init();
+		initMap();
 		return rootView;
 	}
 
-	private void init() {
+	private void initMap() {
 		// TODO Auto-generated method stub
-		if (aMap == null) {
-			aMap = mapView.getMap();
-			aMap.setLocationSource(this);// 设置定位监听
-			aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
-			aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
-
-			LatLng laln = new LatLng(41.8, 123.6);
-			markeroption.title("班车位置");
-			markeroption.position(laln);
-			markeroption.visible(true);
-			marker = aMap.addMarker(markeroption);
-			marker.showInfoWindow();
+		if (map == null) {
+			map = mapView.getMap();
+			map.moveCamera(CameraUpdateFactory.newCameraPosition(SHENYANG));
+			// myMap.setLocationSource(this);// 设置定位监听
+			// myMap.getUiSettings().setMyLocationButtonEnabled(true);//
+			// 设置默认定位按钮是否显示
+			// myMap.setMyLocationEnabled(true);//
+			// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+			drvierMarker = map.addMarker(driverMarkerOption);
+			myMarker = map.addMarker(userMarkerOption);
 		}
+		if (mapLocationManager == null) {
+			mapLocationManager = LocationManagerProxy
+					.getInstance(getActivity());
+			// 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+			// 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用removeUpdates()方法来取消定位请求
+			// 在定位结束后，在合适的生命周期调用destroy()方法
+			// 其中如果间隔时间为-1，则定位只定一次
+			// 在单次定位情况下，定位无论成功与否，都无需调用removeUpdates()方法移除请求，定位sdk内部会移除
+			mapLocationManager.requestLocationData(
+					LocationProviderProxy.AMapNetwork, 10 * 1000, 10, this);
+			// mAMapLocationManager.setGpsEnable(false);
+		}
+
 	}
 
 	/**
@@ -75,53 +155,23 @@ public class BusLocationFragment extends Fragment implements LocationSource,
 	 */
 	@Override
 	public void onLocationChanged(AMapLocation amapLocation) {
-		if (mListener != null && amapLocation != null) {
-			if (amapLocation != null
-					&& amapLocation.getAMapException().getErrorCode() == 0) {
-				mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
-
-				Log.i("aaa",
-						"provider" + amapLocation.getProvider() + "\nLa"
-								+ amapLocation.getLatitude() + "lg"
-								+ amapLocation.getLongitude());
-
-			} else {
-				Log.e("AmapErr", "Location ERR:"
-						+ amapLocation.getAMapException().getErrorCode());
-			}
+		if (amapLocation != null
+				&& amapLocation.getAMapException().getErrorCode() == 0) {
+			// mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+			userLatLng = new LatLng(amapLocation.getLatitude(),
+					amapLocation.getLongitude());
+			myMarker.setPosition(userLatLng);
+			userLocMap.put("lat", String.valueOf(userLatLng.latitude));
+			userLocMap.put("lng", String.valueOf(userLatLng.longitude));
+			userLocMap.put("rout", "1");
+			isUserlocValid = true;
+			ToastUtil.show(getActivity(), "我的位置定位成功" + userLatLng.toString());
+			Log.i("userLatLng", userLocMap.toString());
+		} else {
+			Log.e("AmapErr", "Location ERR:"
+					+ amapLocation.getAMapException().getErrorCode());
 		}
-	}
 
-	/**
-	 * 激活定位
-	 */
-	@Override
-	public void activate(OnLocationChangedListener listener) {
-		mListener = listener;
-		if (mAMapLocationManager == null) {
-			mAMapLocationManager = LocationManagerProxy
-					.getInstance(getActivity());
-			// 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
-			// 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用removeUpdates()方法来取消定位请求
-			// 在定位结束后，在合适的生命周期调用destroy()方法
-			// 其中如果间隔时间为-1，则定位只定一次
-			// 在单次定位情况下，定位无论成功与否，都无需调用removeUpdates()方法移除请求，定位sdk内部会移除
-			mAMapLocationManager.requestLocationData(
-					LocationProviderProxy.AMapNetwork, 10 * 1000, 10, this);
-		}
-	}
-
-	/**
-	 * 停止定位
-	 */
-	@Override
-	public void deactivate() {
-		mListener = null;
-		if (mAMapLocationManager != null) {
-			mAMapLocationManager.removeUpdates(this);
-			mAMapLocationManager.destroy();
-		}
-		mAMapLocationManager = null;
 	}
 
 	/**
@@ -131,6 +181,8 @@ public class BusLocationFragment extends Fragment implements LocationSource,
 	public void onResume() {
 		super.onResume();
 		mapView.onResume();
+		startMyTimer();
+		Log.i("zzz", "fra onResume....startMyTimer");
 	}
 
 	/**
@@ -141,15 +193,16 @@ public class BusLocationFragment extends Fragment implements LocationSource,
 		super.onPause();
 		mapView.onPause();
 		deactivate();
+		Log.i("zzz", "fra onPause");
 	}
 
-	/**
-	 * 方法必须重写
-	 */
 	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		mapView.onSaveInstanceState(outState);
+	public void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		timerTask.cancel();
+		timer.purge();
+		Log.i("zzz", "fra onStop");
 	}
 
 	/**
@@ -159,6 +212,16 @@ public class BusLocationFragment extends Fragment implements LocationSource,
 	public void onDestroy() {
 		super.onDestroy();
 		mapView.onDestroy();
+		Log.i("zzz", "fra onDestroy");
+	}
+
+	/**
+	 * 方法必须重写
+	 */
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		mapView.onSaveInstanceState(outState);
 	}
 
 	@Override
@@ -183,6 +246,69 @@ public class BusLocationFragment extends Fragment implements LocationSource,
 	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
 		// TODO Auto-generated method stub
 
+	}
+
+	private boolean requestDriverLoc() {
+		// TODO Auto-generated method stub
+		try {
+			JSONObject jsonObj = new JSONObject(HttpUtil.postRequest("url",
+					userLocMap));
+			if (jsonObj.get("flag").equals("true")) {
+				driverLocDes = jsonObj.getString("desc");
+				busLatLng = new LatLng(
+						Double.valueOf(jsonObj.getString("lat")),
+						Double.valueOf(jsonObj.getDouble("lng")));
+				drvierMarker.setTitle(driverLocDes);
+				drvierMarker.setPosition(busLatLng);
+				drvierMarker.showInfoWindow();
+				// map.animateCamera(CameraUpdateFactory.newLatLng(busLatLng));
+				Log.i("receivejson", jsonObj.toString());
+				Log.i("aaaa", Thread.currentThread() + " a " + a++);
+				return true;
+			}
+			return false;
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	private void startMyTimer() {
+		// TODO Auto-generated method stub
+		if (timer != null) {
+			if (timerTask != null) {
+				timerTask.cancel(); // 将原任务从队列中删除
+			}
+		}
+		/**
+		 * 注意： 每次放任务都要新建一个任务对象，否则出现一下错误： ERROR/AndroidRuntime(11761):
+		 * java.lang.IllegalStateException: TimerTask is scheduled already
+		 * 所以同一个定时器任务只能被放置一次
+		 */
+		timerTask = new MyTimerTask(); // 新建一个任务（必须）
+		timer.scheduleAtFixedRate(timerTask, 3000, 3000);
+	}
+
+	/**
+	 * 关闭定位
+	 */
+	public void deactivate() {
+		if (mapLocationManager != null) {
+			mapLocationManager.removeUpdates(this);
+			mapLocationManager.destroy();
+		}
+		mapLocationManager = null;
+	}
+
+	@Override
+	public void onClick(DialogInterface arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
---
+++ src/cn/cas/sict/utils/HttpUtil.java
@@ -0,0 +1,88 @@
+package cn.cas.sict.utils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.CoreConnectionPNames;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class HttpUtil {
+	// 创建HttpClient对象
+	public static HttpClient httpClient = new DefaultHttpClient();
+	public static final String BASE_URL = "http://Todolist.f3322.net/JSON_Service/";
+	static String result;
+	static Double lat = 41.7;
+	static Double lng = 123.6;
+
+	/**
+	 * @param url
+	 *            发送请求的URL
+	 * @param params
+	 *            请求参数
+	 * @return 服务器响应字符串
+	 * @throws Exception
+	 */
+	public static String postRequest(final String url,
+			final Map<String, String> rawParams) throws Exception {
+		FutureTask<String> task = new FutureTask<String>(
+				new Callable<String>() {
+					@Override
+					public String call() throws Exception {
+						// 创建HttpPost对象。
+						HttpPost post = new HttpPost(url);
+						post.setHeader("Content-Type",
+								"application/x-www-form-urlencoded; charset=utf-8");
+						JSONObject json = new JSONObject(rawParams);
+						post.getParams().setIntParameter(
+								CoreConnectionPNames.CONNECTION_TIMEOUT, 2000);
+						post.setEntity(new StringEntity(json.toString(),
+								HTTP.UTF_8));
+
+//						Log.i("sendjsonstring", json.toString());
+
+//						// 发送POST请求
+//						HttpResponse httpResponse;
+//						httpResponse = httpClient.execute(post);
+//						if (httpResponse.getStatusLine().getStatusCode() == 200) {
+//							// 获取服务器响应字符串
+//							result = EntityUtils.toString(httpResponse
+//									.getEntity());
+//							return result;
+//						} else {
+//							return null;
+//						}
+						
+						Map<String,String> m = new HashMap<String, String>();
+						m.put("lat", ""+lat); lat = lat + 0.0001;
+						m.put("lng", ""+lng);  lng = lng + 0.0001;
+						m.put("desc", "zzzzzzaaa");
+						m.put("flag", "true");
+						JSONObject js = new JSONObject(m);
+						return js.toString();
+						
+					}
+				});
+		new Thread(task).start();
+		Log.i("task.get() ", task.get()+"")  ;
+		return task.get();
+	}
+}
