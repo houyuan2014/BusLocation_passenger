2f859930170580e7acd1bdd936a2e806e7ceefad
hy <houyuan@qq.com>
2015-10-08 20:18:33 +0800

    添加导航图标显示，添加sharedpreferences文件描述

--- src/cn/cas/sict/BusLocation_passenger/LoginActivity.java
+++ src/cn/cas/sict/BusLocation_passenger/LoginActivity.java
@@ -1,5 +1,10 @@
 package cn.cas.sict.BusLocation_passenger;
 
+import com.amap.api.location.AMapLocalWeatherForecast;
+import com.amap.api.location.AMapLocalWeatherListener;
+import com.amap.api.location.AMapLocalWeatherLive;
+import com.amap.api.location.LocationManagerProxy;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -7,21 +12,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements AMapLocalWeatherListener {
 	EditText tel, psw;
-	String tel1,psw1;
+	TextView weather;
+	String tel1, psw1;
 	SharedPreferences sharedpreferences;
 	SharedPreferences.Editor editor;
+	LocationManagerProxy locManagerProxy;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		sharedpreferences = getSharedPreferences("userconfig",
-				MODE_PRIVATE);
+		locManagerProxy = LocationManagerProxy.getInstance(this);
+		locManagerProxy.requestWeatherUpdates(
+				LocationManagerProxy.WEATHER_TYPE_LIVE, this);
+		sharedpreferences = getSharedPreferences("userconfig", MODE_PRIVATE);
 		editor = sharedpreferences.edit();
-		editor.putBoolean("hasvibrate", false).commit(); //必须要commit()才能生效
+		editor.putBoolean("hasvibrate", false).commit(); // 必须要commit()才能生效
+		weather = (TextView) findViewById(R.id.tv_weather);
 		Button zhuce = (Button) findViewById(R.id.bt_logup);
 		zhuce.setOnClickListener(new View.OnClickListener() {
 
@@ -72,6 +83,7 @@ public class LoginActivity extends Activity {
 
 	/**
 	 * 请求登录
+	 * 
 	 * @param tel1
 	 * @param psw1
 	 * @return
@@ -79,6 +91,32 @@ public class LoginActivity extends Activity {
 	private boolean loginpro(String tel1, String psw1) {
 		// TODO Auto-generated method stub
 		return true;
+	}
+
+	@Override
+	public void onWeatherForecaseSearched(AMapLocalWeatherForecast arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onWeatherLiveSearched(AMapLocalWeatherLive localWeatherLive) {
+		// TODO Auto-generated method stub
+		if (localWeatherLive != null
+				&& localWeatherLive.getAMapException().getErrorCode() == 0) {
+			weather.setText(localWeatherLive.getCity() + "  "
+					+ localWeatherLive.getWeather() + "  "
+					+ localWeatherLive.getTemperature() + "℃  "
+					+ localWeatherLive.getWindDir() + "风  "
+					+ localWeatherLive.getWindPower() + "级");
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		locManagerProxy.destroy();
 	}
 
 }
